cc562ac2143eadc6e6f7254ae3f7ffbad5c37cea
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2023-02-02 18:46:29 +0100

    Update Phonebook

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/contacts-operation';
 import { selectContacts } from 'redux/contacts/contacts-selector';
 import { selectFilter } from 'redux/filter/filter-selector';
+import { selectError } from 'redux/contacts/contacts-selector';
 import { ContactItem } from './ContactItem';
 import { List, SimpleGrid} from '@chakra-ui/react';
 
 
 export const ContactList = () => {
   const [userToUpdate, setUserToUpdate] = useState(null);
+  const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectFilter);
+  const error = useSelector(selectError)
   const visibleContacts = getVisibleContatcts();
+
+ useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+
   function getVisibleContatcts() {
     const normalizedFilter = filter.toLocaleLowerCase();
     return contacts.filter(contact =>
@@ -26,7 +37,9 @@ export const ContactList = () => {
   };
 
   return (
-    <List tm='10'>
+<>
+    {!error && (
+      <List tm='10'>
       <SimpleGrid
           p="10px"
           w="100%"
@@ -41,6 +54,7 @@ export const ContactList = () => {
           )}
         </SimpleGrid>
     </List>
-    
+    )}
+    </>
   );
 };
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,24 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectIsLoading } from 'redux/contacts/contacts-selector';
-import { selectToken } from 'redux/auht/auth-selector';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/contacts/contacts-operation';
 import { ContactForm } from 'components/ContactForm/ContactForm'; 
 import { Filter } from 'components/Filter/Filter'; 
 import { ContactList } from 'components/ContactList/ContactList'; 
 import { Box, Flex, Spinner } from '@chakra-ui/react';
 
 export const ContactsPage = () => {
-  const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
-  const token = useSelector(selectToken);
   
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch, token]);
-
   return (
-<>
 <Flex width="full" align="center" justifyContent="center" flexDirection='column'>
       <Box
         p={8}
@@ -32,9 +22,8 @@ export const ContactsPage = () => {
         </Box>
            
       <Filter />
-      {isLoading && <Spinner color="purple.800" size='xl' />}
-        {!isLoading && token && <ContactList />}
+        {isLoading && <Spinner color="purple.800" size='xl' />}
+        <ContactList />
         </Flex>
-     </>
   );
 };
